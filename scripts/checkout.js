92cf9530cc687c1ca7bc67202cb46d5b9ba49aea
kerem.oguz <kerem.oguz207@gmail.com>
2024-08-18 23:44:28 +0300

    make delivery options interactive

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import { cart, removeFromCart } from "../data/cart.js";
+import { cart, removeFromCart, updateDeliveryOption } from "../data/cart.js";
 import { products } from "../data/products.js";
 import { formatCurrency } from "./utils/money.js";
 import dayjs from 'https://unpkg.com/dayjs@1.11.10/esm/index.js';
@@ -68,13 +68,12 @@ cart.forEach((cartItem) => {
             </div>
 
             <div class="delivery-options">
-            <div class="delivery-options-title">
-                Choose a delivery option:
-            </div>
-            <div>
-               ${deliveryOptionsHTML(matchingProduct, cartItem)}
-            </div>
-            
+                <div class="delivery-options-title">
+                    Choose a delivery option:
+                </div>
+                <div>
+                ${deliveryOptionsHTML(matchingProduct, cartItem)}
+                </div>
             </div>
         </div>
     </div>
@@ -100,7 +99,9 @@ function deliveryOptionsHTML(matchingProduct, cartItem) {
             cartItem.deliveryOptionId;
         html +=
         `  
-        <div class="delivery-option">
+        <div class="delivery-option js-delivery-option"
+            data-product-id = ${matchingProduct.id}
+            data-delivery-option-id = ${deliveryOption.id}>
                 <input type="radio"
                     ${isChecked ? 'checked' : ''}
                     class="delivery-option-input"
@@ -131,5 +132,13 @@ document.querySelectorAll('.js-delete-link')
                 `.js-cart-item-container-${productId}`);
             console.log(container);
             container.remove();
+        });
+    });
+
+document.querySelectorAll('.js-delivery-option')
+    .forEach((element) => {
+        element.addEventListener('click', ()=> {
+            const {productId, deliveryOptionId} = element.dataset;
+            updateDeliveryOption(productId, deliveryOptionId);
         });
     });
